80058c6b4b682fce055e6374313734425af1f0bd
Mario Astudillo <marastfie@alum.us.es>
2025-12-02 19:33:28 +0100

    fix: arreglos de la parte del perfil

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Product, Service, Tarifa, Cita, User
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+	list_display = ("name", "price")
+	search_fields = ("name",)
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+	list_display = ("title", "subtitle", "imagen_der")
+	search_fields = ("title", "subtitle")
+
+
+@admin.register(Tarifa)
+class TarifaAdmin(admin.ModelAdmin):
+	list_display = ("service", "title", "price")
+	list_filter = ("service",)
+
+
+@admin.register(Cita)
+class CitaAdmin(admin.ModelAdmin):
+	list_display = ("user", "service", "tarifa", "date", "time", "estado")
+	list_filter = ("estado", "service")
+	search_fields = ("user__username",)
+
+
+@admin.register(User)
+class ProfileAdmin(admin.ModelAdmin):
+	list_display = ("username", "first_name", "last_name", "mail")
+	search_fields = ("username", "mail", "first_name", "last_name")
